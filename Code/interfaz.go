59c33c6e3504567c85f22dbd7afe2ec38dc24f0c
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2026-02-19 20:22:54 -0400

    feat: corrigiendo UX

--- Code/interfaz.go
+++ Code/interfaz.go
@@ -3,23 +3,14 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
 )
 
 func EsperarParaGrabar(receptorControl *net.UDPConn) bool {
-	const cuentaSegundos = 3
-	fmt.Println()
-	for i := cuentaSegundos; i > 0; i-- {
-		fmt.Println("Iniciando grabación en", i)
-		time.Sleep(1 * time.Second)
-		
-		if VerificarComandoDetener(receptorControl) {
-			fmt.Println("\nComando 'CMD:STOP' recibido — deteniendo cliente")
-			return false
-		}
+	if receptorControl != nil && VerificarComandoDetener(receptorControl) {
+		fmt.Println("Comando 'CMD:STOP' recibido — deteniendo cliente")
+		return false
 	}
-
-	fmt.Println()
+	
 	return true
 }
 
